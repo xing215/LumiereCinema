ac3cf3c3e8b0d20266404c407f6f8bcf793c036f
gugOfBoat <ttlam23@clc.fitus.edu.vn>
2025-06-29 21:22:10 +0700

    LC-151: feat - Implement all data models and schemas

---
+++ src/back-end/models/Movie.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+
+  title: { type: String, required: true, unique: true, trim: true },
+  posterURL: { type: String, required: true },
+  description: { type: String, required: true, trim: true },
+  trailerURL: { type: String, default: '' },
+
+  releaseDate: { type: Date, required: true },
+  duration: { type: Number, required: true }, // phút
+  genre: { type: [String], required: true },
+  director: { type: String, required: true, trim: true },
+  cast: { type: [String], required: true },
+  language: { type: String, required: true },
+
+  status: { 
+    type: String, 
+    required: true, 
+    enum: ['Now Showing', 'Upcoming', 'Archived'], 
+    default: 'Upcoming' 
+  },
+  ageRating: { 
+    type: String, 
+    required: true, 
+    enum: ['P', 'C13', 'C16', 'C18'], 
+    default: 'P'
+  },
+  
+  ratingsAverage: { type: Number, default: 0, min: 0, max: 5 },
+  ratingsQuantity: { type: Number, default: 0 },
+  
+}, { timestamps: true }); 
+
+movieSchema.index({ status: 1, releaseDate: -1 });
+module.exports = mongoose.model('Movie', movieSchema);
