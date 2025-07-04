99dc8d8966d48aa9e0104e43b6f93c4d627956f4
Nhut Anh <andree.forwork@gmail.com>
2025-07-04 21:52:44 +0700

    Push changes

--- src/front-end/src/layouts/LandingPage/Header.jsx
+++ src/front-end/src/layouts/LandingPage/Header.jsx
@@ -3,7 +3,7 @@ import SearchButton from '../../components/buttons/searchButton.jsx';
 import Logo from "../../components/buttons/logoButton.jsx";
 
 const Header = () => (
-    <div className="fixed top-0 z-100 w-screen bg-transparent bg-opacity-100">
+    <header className="fixed top-0 z-100 w-screen bg-transparent bg-opacity-100 overflow-y-visible no-scrollbar">
         <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[calc(100vw+2rem)] lg:h-20 sm:h-10 h-8 bg-gradient-to-b from-slate-950 via-slate-950 to-transparent z-20" />
         <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[calc(100vw+2rem)] lg:h-30 sm:h-20 h-15 bg-gradient-to-b from-slate-950 via-slate-950 to-transparent lg:blur-md sm:blur-lg blur-sm z-20" />
         <div className="relative overflow-x-hidden flex justify-center items-center xl:gap-10 lg:gap-5 md:gap-3 flex-nowrap content-center xl:pt-8 lg:pt-5.5 md:pt-3 pt-2 max-w-screen mx-auto">
@@ -13,7 +13,7 @@ const Header = () => (
             <NavButton name="Login/Register" />
             <SearchButton />
             </div>
-    </div>
+    </header>
 );
 
 export default Header;
