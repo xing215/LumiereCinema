80bcd167573412969682d602a79c5755a11ff6ed
Gia Thành <71538542+xing215@users.noreply.github.com>
2025-07-08 22:05:33 +0700

    Typo fix

--- src/front-end/src/components/UI/locationCard.jsx
+++ src/front-end/src/components/UI/locationCard.jsx
@@ -1,6 +1,6 @@
 const LocationCard = ({ branchName, distance, nowShowing }) => {
     return (
-        <div className="relative z-10 w-full xl:h-[15%] md:h-[25%] h-[20%] rounded-xl mix-blend-color-dodge xl:bg-zinc-300/20+ bg-zinc-300/10
+        <div className="relative z-10 w-full xl:h-[15%] md:h-[25%] h-[20%] rounded-xl mix-blend-color-dodge xl:bg-zinc-300/20 bg-zinc-300/10
         xl:p-4 lg:p-3 p-2 pt-1 md:pt-2
         xl:mb-4 lg:mb-3 mb-2">
             <h2 className="text-white font-bold font-libre-franklin
