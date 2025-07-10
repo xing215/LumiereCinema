eb52dc6516345e9640f8df3680fbc803c338179a
Nhut Anh <andree.forwork@gmail.com>
2025-07-11 02:45:09 +0700

    Install preetier and run it to re-format front-end code

--- src/front-end/src/pages/staff/ScheduleManagePage.jsx
+++ src/front-end/src/pages/staff/ScheduleManagePage.jsx
@@ -1,7 +1,7 @@
 import UploadCSVButton from '../../components/buttons/scheduleMange/uploadCsvButton.jsx';
 import AddScheduleButton from '../../components/buttons/scheduleMange/addScheduleButton.jsx';
 import { Download } from 'lucide-react';
-import {Circle} from "lucide-react";
+import { Circle } from 'lucide-react';
 
 const DownloadTemplateButton = () => {
     return (
@@ -15,25 +15,23 @@ const DownloadTemplateButton = () => {
 const DateChosenButton = () => {
     return (
         <button className="relative z-20 flex h-8 items-center gap-2">
-
-            <button className="font-unbounded h-9 px-5 rounded-xl bg-white text-lg text-slate-950">07/10/2025</button>
-
+            <button className="font-unbounded h-9 rounded-xl bg-white px-5 text-lg text-slate-950">07/10/2025</button>
         </button>
     );
 };
 
 const SelectBranchButton = () => {
     return (
-        <button className="absolute w-96 h-9 bottom-5 left-1/2 transform -translate-x-1/2">
-            <div className="w-96 h-9 left-0 top-0 absolute bg-white rounded-xl shadow-[inset_0px_0px_50px_3px_rgba(3,5,28,1.00)]" />
-            <div className="absolute text-center top-1/2 left-1/2 transform -translate-1/2 justify-start text-white text-base text-nowrap font-bold font-unbounded">LUMIERE CINEMA CAO THẮNG</div>
+        <button className="absolute bottom-5 left-1/2 h-9 w-96 -translate-x-1/2 transform">
+            <div className="absolute top-0 left-0 h-9 w-96 rounded-xl bg-white shadow-[inset_0px_0px_50px_3px_rgba(3,5,28,1.00)]" />
+            <div className="font-unbounded absolute top-1/2 left-1/2 -translate-1/2 transform justify-start text-center text-base font-bold text-nowrap text-white">LUMIERE CINEMA CAO THẮNG</div>
         </button>
-    )
-}
+    );
+};
 
 const Schedule = () => {
     return (
-        <div className="absolute bottom-1/10  flex h-[67%] w-screen flex-col items-end pr-[4%] z-20 overflow-x-hidden">
+        <div className="absolute bottom-1/10 z-20 flex h-[67%] w-screen flex-col items-end overflow-x-hidden pr-[4%]">
             {/*Calendar*/}
             <div className="relative z-10 h-[95%] w-[90%]">
                 <div className="flex justify-between px-[1px] py-1">
@@ -56,18 +54,17 @@ const Schedule = () => {
                 </div>
             </div>
 
-            <div className="absolute top-[5%] left-[4%] w-[4%] h-[95%] py-3 bg-black/20 flex flex-col gap-3">
-                    {Array.from({ length: 5 }, (_, index) => {
-                        return (
-                            <>
-                                <div key={index} className="relative w-full h-[10%] ">
-                                    <span className="absolute top-1/2 left-1/2 transform -translate-1/2 text-bold font-unbounded text-2xl font-black">{index}</span>
-                                </div>
-                            </>
-                        )
-                    })}
+            <div className="absolute top-[5%] left-[4%] flex h-[95%] w-[4%] flex-col gap-3 bg-black/20 py-3">
+                {Array.from({ length: 5 }, (_, index) => {
+                    return (
+                        <>
+                            <div key={index} className="relative h-[10%] w-full">
+                                <span className="text-bold font-unbounded absolute top-1/2 left-1/2 -translate-1/2 transform text-2xl font-black">{index}</span>
+                            </div>
+                        </>
+                    );
+                })}
             </div>
-
         </div>
     );
 };
@@ -77,23 +74,16 @@ const ScheduleManagePage = () => {
         <div className="relative h-screen w-screen overflow-hidden bg-zinc-300/70">
             <div className="font-unbounded absolute top-5 left-1/6 z-10 justify-start text-5xl font-bold text-black">Schedule</div>
 
-            <div className="absolute xl:top-1/20 lg:top-1/10 right-1/12 z-10 flex items-end gap-4">
+            <div className="absolute right-1/12 z-10 flex items-end gap-4 lg:top-1/10 xl:top-1/20">
                 <AddScheduleButton />
                 <div className="flex flex-col items-center">
-
                     <DownloadTemplateButton />
                     <UploadCSVButton />
                 </div>
                 <DateChosenButton />
             </div>
 
-
-
-            <div className="absolute left-1/2 transform -translate-x-1/2 bg-black/10 xl:rounded-3xl rounded-xl
-            xl:bottom-1/10 lg:bottom-1/10
-            xl:h-[70%] lg:h-[70%]
-            w-[95%]
-            z-4 "></div>
+            <div className="absolute left-1/2 z-4 w-[95%] -translate-x-1/2 transform rounded-xl bg-black/10 lg:bottom-1/10 lg:h-[70%] xl:bottom-1/10 xl:h-[70%] xl:rounded-3xl"></div>
 
             <Schedule />
 
