2dc6eb8bf1a79041cd41835d9d1050b94a8be8f2
Nhut Anh <andree.forwork@gmail.com>
2025-07-17 15:20:41 +0700

    Finish manage movie screen

--- src/front-end/src/components/buttons/Staff/AddButton.jsx
+++ src/front-end/src/components/buttons/Staff/AddButton.jsx
@@ -1,9 +1,9 @@
-const AddButton = () => {
+const AddButton = (props) => {
     return (
-        <button className="font-unbounded absolute top-1/6 right-1/10 z-20 flex h-7 w-52 items-center justify-center rounded-xl bg-pink-400 text-sm font-bold text-white shadow-[inset_0px_0px_50px_3px_rgba(155,47,255,1.00)] hover:cursor-pointer">
-            ADD PROMOTION
+        <button className="font-unbounded relative z-20 flex h-8 w-44 items-center justify-center rounded-md bg-pink-400 text-sm font-bold text-white shadow-[inset_0px_0px_50px_3px_rgba(155,47,255,1.00)] hover:cursor-pointer sm:rounded-lg lg:rounded-xl">
+            {props?.text}
         </button>
     );
 };
 
-export default AddButton;
+export default AddButton
