f1ce56e4e1c83a6cee7b3b0384c1f9ef2072d5b7
Hồng Thanh <thanhngo2707@gmail.com>
2025-07-23 11:55:33 +0700

    Lc 308 back end wishlist history (#64)

---
+++ src/back-end/routes/user.route.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getProfile,
+  updateProfile,
+  rateMovie,
+  getRatingMovie,
+  addToWishlist,
+  removeFromWishlist,
+  getWishlist,
+  getWatchHistory,
+  removeFromWatchHistory
+} = require('../controllers/user.controller');
+
+const { protect, restrictTo } = require('../middlewares/auth.middleware.js');
+
+// // Profile
+// router.get('/me', getProfile);
+// router.put('/me', updateProfile);
+
+// // Movie rating
+// router.post('/rate', rateMovie);
+// router.get('/rating/:movieId', getRatingMovie);
+
+// Wishlist
+router.post('/wishlist/:movieId', protect, restrictTo('customer'), addToWishlist);
+router.delete('/wishlist/:movieId', protect, restrictTo('customer'), removeFromWishlist);
+router.get('/wishlist', protect, restrictTo('customer'), getWishlist);
+
+// Watch history
+router.get('/watch-history', protect, restrictTo('customer'), getWatchHistory);
+router.delete('/watch-history/:ticketId', protect, restrictTo('administrator'), removeFromWatchHistory);
+
+module.exports = router;
