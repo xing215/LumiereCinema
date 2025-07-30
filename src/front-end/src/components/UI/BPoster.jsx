66f59d96d015fa34b5a21b65e9208fcac0242bc4
Gia Thành <71538542+xing215@users.noreply.github.com>
2025-07-30 19:46:56 +0700

    fallback image for buy ticket

--- src/front-end/src/components/UI/BPoster.jsx
+++ src/front-end/src/components/UI/BPoster.jsx
@@ -1,6 +1,8 @@
+import fallbackImg from '@assets/img/PosterNotFound.png';
+
 const BPoster = ({ Pics }) => (
     <div className="relative mx-auto aspect-[300/470] max-h-[470px] overflow-hidden rounded-xl md:aspect-auto md:h-full md:max-h-[470px] md:w-auto md:max-w-[300px] md:min-w-[200px]">
-        <img src={Pics} className="h-full w-full rounded-xl object-cover" alt="Poster" />
+        <img src={Pics} className="h-full w-full rounded-xl object-cover" alt="Poster" onError={e => { e.target.onerror = null; e.target.src = fallbackImg; }} />
     </div>
 );
 
